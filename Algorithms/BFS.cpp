d420089b44ed2c1f200163549e81dc1675adfbb9
DhvanilP <dhvanilhparikh@gmail.com>
2019-03-26 23:56:41 +0530

    new fle added

--- Algorithms/BFS.cpp
+++ Algorithms/BFS.cpp
@@ -1,53 +1,48 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-  int nodes,edges;
-  cin>>nodes;
-  cin>>edges;
+int main()
+{
+  int nodes, edges;
+  cin >> nodes;
+  cin >> edges;
 
-  std::vector<int>v[10];
+  std::vector<int> v[10];
 
-  for(int i=1;i<=edges;i++)
+  for (int i = 1; i <= edges; i++)
   {
-    int x,y;
-    cin>>x;
-    cin>>y;
+    int x, y;
+    cin >> x;
+    cin >> y;
     v[x].push_back(y);
     v[y].push_back(x);
   }
 
-bool visit[11];
-for(int i=0;i<11;i++)
-visit[i]=false;
+  bool visit[11];
+  for (int i = 0; i < 11; i++)
+    visit[i] = false;
 
-queue<int>s;
+  queue<int> s;
 
-s.push(1);
-visit[0]=true;
-visit[1]=true;
+  s.push(1);
+  visit[0] = true;
+  visit[1] = true;
 
-while(!(s.empty()))
-{
-  int t=s.front();
-  cout << t<<" ";
-  s.pop();
-
-  for(int i=0;i<v[t].size();i++)
+  while (!(s.empty()))
   {
-    if(visit[v[t][i]]==false)
+    int t = s.front();
+    cout << t << " ";
+    s.pop();
+
+    for (int i = 0; i < v[t].size(); i++)
     {
-      visit[v[t][i]]=true;
-      s.push(v[t][i]);
+      if (visit[v[t][i]] == false)
+      {
+        visit[v[t][i]] = true;
+        s.push(v[t][i]);
+      }
     }
   }
-}
-
-
-
-
-
-
 
   return 0;
 }
